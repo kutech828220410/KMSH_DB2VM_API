61cb359e209bf37dab89dde32deb8c32b113fa1b
WPC <WPC@KMSH-PHARMACY>
2023-09-14 14:50:19 +0800

    加入專案檔案。

---
+++ Controller/TestController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IBM.Data.DB2.Core;
+using System.Data;
+using System.Configuration;
+using Basic;
+using Oracle.ManagedDataAccess.Client;
+using ServiceReference;
+namespace DB2VM
+{
+    [Route("dbvm/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+
+
+        // GET api/values
+        [HttpGet]
+        async public Task<string> Get(string barbode)
+        {
+
+            ServiceReference.ADCMedicineCabinetWCFServiceClient client = new ADCMedicineCabinetWCFServiceClient();
+           
+            string json = await client.MEDCABINET_DataAsync(barbode);
+            return json;
+
+
+        }
+
+
+    }
+}
